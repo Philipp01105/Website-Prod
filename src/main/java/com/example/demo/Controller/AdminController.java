122d57cfca0103b9e0e4fb23ac4b79442068beeb
Philipp01105 <96082163+Philipp01105@users.noreply.github.com>
2025-02-03 11:23:51 +0100

    Outsourced admin requests to AdminController Outsourced helper functions(secureSiteGet() and getCurrentUsername()) to ControllerHelper in Utils directory

---
+++ src/main/java/com/example/demo/Controller/AdminController.java
@@ -0,0 +1,139 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Entities.Blog;
+import com.example.demo.Entities.Contact;
+import com.example.demo.Entities.User;
+import com.example.demo.Entities.Wiki;
+import com.example.demo.Repositories.BlogRepository;
+import com.example.demo.Repositories.ContactRepository;
+import com.example.demo.Repositories.UserRepository;
+import com.example.demo.Repositories.WikiRepository;
+import com.example.demo.Services.MarkdownConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import static com.example.demo.Util.ControllerHelper.getCurrentUsername;
+import static com.example.demo.Util.ControllerHelper.secureSiteGet;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private ResourcePatternResolver resourcePatternResolver;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Autowired
+    private WikiRepository wikiRepository;
+
+    @Autowired
+    private BlogRepository blogRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/")
+    public ModelAndView admin(Model model) {
+        return secureSiteGet(model, "Admin-usages/admin", null, null, null);
+    }
+
+    @GetMapping("/add-blog")
+    public ModelAndView addSectionPage(Model model) {
+        return secureSiteGet(model, "Admin-usages/add-blog", null, null, null);
+    }
+
+    @GetMapping("/admin/add-wiki")
+    public ModelAndView addWikiPage(Model model) throws IOException {
+        Resource[] resources = resourcePatternResolver.getResources("classpath:static/*.*");
+        List<String> images = Arrays.stream(resources)
+                .map(Resource::getFilename).filter(Objects::nonNull)
+                .filter(filename -> filename.toLowerCase().endsWith(".jpg") ||
+                        filename.toLowerCase().endsWith(".png") ||
+                        filename.toLowerCase().endsWith(".gif"))
+                .toList();
+
+        model.addAttribute("images", images);
+        return new ModelAndView("Admin-usages/add-wiki");
+    }
+
+    @GetMapping("/admin/manage-roles")
+    public ModelAndView manageRoles(Model model) {
+        return secureSiteGet(model, "Admin-usages/manage-roles", "users", userRepository, User.class);
+    }
+
+    @GetMapping("/admin/reset-password")
+    public ModelAndView resetPassword(Model model) {
+        return secureSiteGet(model, "Admin-usages/reset-password", null, null, null);
+    }
+
+    @GetMapping("/admin/reports")
+    public ModelAndView reports(Model model) {
+        return secureSiteGet(model, "Admin-usages/reports", "contacts", contactRepository, Contact.class);
+    }
+
+/*---------------------------------Post Methods---------------------------------*/
+
+    @PostMapping("/add-blog")
+    public ModelAndView addSection(@RequestParam("title") String title, @RequestParam("content") String content) {
+        Blog section = new Blog();
+        section.setTitle(title);
+        MarkdownConverter markdownConverter = new MarkdownConverter();
+        content = markdownConverter.convertToHtml(content);
+        section.setContent(content);
+        section.setTimestamp(LocalDateTime.now());
+        blogRepository.save(section);
+        return new ModelAndView("redirect:/admin");
+    }
+
+    @PostMapping("/add-wiki")
+    public ModelAndView addWiki(@RequestParam("title") String title, @RequestParam("content") String content, @RequestParam("url") String url) {
+        Wiki wiki = new Wiki();
+        wiki.setWikiname(title);
+        MarkdownConverter markdownConverter = new MarkdownConverter();
+        content = markdownConverter.convertToHtml(content);
+        wiki.setContent(content);
+        if(url.isEmpty())url = "/original_black@2x.png";
+        wiki.setPicPath(url);
+        wikiRepository.save(wiki);
+        return new ModelAndView("redirect:/admin");
+    }
+
+    @PostMapping("/manage-roles")
+    public ModelAndView handleManageRoles(
+            @RequestParam("username") String username,
+            @RequestParam("role") String role) {
+        User user = userRepository.findByUsername(username);
+        if (user != null) {
+            user.setRole(role);
+            userRepository.save(user);
+        }
+        return new ModelAndView("redirect:/admin");
+    }
+
+    @PostMapping("/reset-password")
+    public ModelAndView handleResetPassword(
+            @RequestParam("oldPassword") String oldPassword,
+            @RequestParam("newPassword") String newPassword) {
+        User user = userRepository.findByUsername(getCurrentUsername());
+        if (user != null && passwordEncoder.matches(oldPassword, user.getPassword())) {
+            user.setPassword(passwordEncoder.encode(newPassword));
+            userRepository.save(user);
+        }
+        return new ModelAndView("redirect:/admin");
+    }
+}
---
+++ src/main/java/com/example/demo/Util/ControllerHelper.java
@@ -0,0 +1,54 @@
+package com.example.demo.Util;
+
+import com.example.demo.Entities.Identifiable;
+import com.example.demo.Entities.Timestamped;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+public class ControllerHelper {
+    public static String getCurrentUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails userDetails) {
+            return userDetails.getUsername();
+        }
+        return null;
+    }
+
+    public static <T> ModelAndView secureSiteGet(
+            Model model,
+            String viewPath,
+            String entityListName,
+            JpaRepository<T, Long> repository,
+            Class<T> entityClass) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        boolean isLoggedIn = authentication != null && authentication.isAuthenticated() && !authentication.getPrincipal().equals("anonymousUser");
+        model.addAttribute("isLoggedIn", isLoggedIn);
+        assert authentication != null;
+        model.addAttribute("role", authentication.getAuthorities().toString());
+
+        if(repository != null && entityClass != null && entityListName != null) {
+            List<T> entityList = repository.findAll();
+
+            if (Timestamped.class.isAssignableFrom(entityClass)) {
+                entityList.sort((s1, s2) -> ((Timestamped)s2).getTimestamp()
+                        .compareTo(((Timestamped)s1).getTimestamp()));
+            } else if (Identifiable.class.isAssignableFrom(entityClass)) {
+                entityList.sort((s1, s2) -> ((Identifiable)s2).getId()
+                        .compareTo(((Identifiable)s1).getId()));
+            }
+
+            model.addAttribute(entityListName, entityList);
+        }
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName(viewPath);
+        return modelAndView;
+    }
+}
